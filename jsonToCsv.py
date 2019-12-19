46c2f782567afb6b57933e1f88bf8ef101918ee1
Mahmudul Hasan <mhasan0047@gmail.com>
2019-12-19 01:48:31 -0500

    added record 9 and turned all to csv and also made csv of unique tickets

---
+++ jsonToCsv.py
@@ -0,0 +1,5 @@
+# 1) (Need to know all the tickets we got atm) read from the csv file's first column if it exists and add it to the dictionary 
+# 2) read from a json file
+# 3) put the ticket numbers into a dictionary if it doesnt exist
+# 4) print that object into the csv file
+
