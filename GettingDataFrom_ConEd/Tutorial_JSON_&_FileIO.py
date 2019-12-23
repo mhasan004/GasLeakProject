a8cd2bd22407ddea186318524cf0699007b221de
Mahmudul Hasan <mhasan0047@gmail.com>
2019-12-23 06:24:30 -0500

    json to csv

--- GettingDataFrom_ConEd/Tutorial_JSON_&_FileIO.py
+++ GettingDataFrom_ConEd/Tutorial_JSON_&_FileIO.py
@@ -1,7 +1,8 @@
 import json
 import csv
 
-############################################## WRITE/APPEND to End of File ################################
+
+############################################## 1) WRITE/APPEND to End of File ################################
 # Step 1: Make a dictionary which will be the json file
 data = {}
 # Step 2: Json element
@@ -16,13 +17,67 @@ data['aKey'].append({
     'property3': 'value3',
     'property4': 'value4'
 })
-
 # Step 4: append the json file (the dictionary) to the end of a file. 
-# with open('test.json','a') as outfile:
-#     a = json.dumps(data, sort_keys=True, indent=4)              # turns the dict() into a string. dict() is the same format as JSON. indent=4 to make it look good
-#     outfile.write(a)                                            # append to end of list
-
-############################################## READ FROM JSON FILE #######################################
+# with open('test.json','a') as outf:
+#     a = json.dumps(data, sort_keys=True, indent=4)                        # turns the dict() into a string. dict() is the same format as JSON. indent=4 to make it look good
+#     outf.write(a)                                                         # append to end of list
+############################################## ) READ FROM JSON FILE #######################################
 with open('test.json',"r") as readf:
-    data = json.load(readf)
-print(data["key"])
+    dataj = json.load(readf)
+print(dataj["key1"])
+
+
+
+############################################## x) JSON to CSV USING MY FILE###############################
+########################## pandas:
+# import pandas as pd
+# df = pd.read_json('test.json')
+# df.to_csv("test.csv")  #need to use two csv 
+# df.to_csv("test.csv") 
+######################### one step:
+#pd.read_json('test.json').drop(columns=['LastInspected']).to_csv("test.csv", mode='a', header=False) #append to csv
+
+
+################## older:
+# jsonFileName = "test.json"
+# csvHeader = ["key1", "property1", "property2","property3","property4"]
+# with open(jsonFileName) as readf:
+#     dataStrj = json.loads(readf.read())
+# outf = csv.writer(open("test.csv","w", newline='')) #python2: "wb+". python3: "w", newline=''
+# # Write the CSV header. write the keynames (if its a json obj, include only the inside keys)
+# outf.writerow(csvHeader)    
+# for d in dataStrj:
+#     outf.writerow([
+#         dataStrj['key1'],
+#     ])
+################### older:
+# jsonFileName = "test.json"
+# csvHeader = ["Ticket Number","Longitude","Latitude","Zipcode","Classification","Date Reported"]
+# outf = csv.writer(open("test.csv","w", newline='')) #python2: "wb+". python3: "w", newline=''
+# # Write the CSV header. write the keynames (if its a json obj, include only the inside keys)
+# outf.writerow(csvHeader)    
+# with open(jsonFileName) as readf:
+#     dataStrj = json.loads(readf.read())
+# for d in dataStrj:
+#     outf.writerow([
+#         dataStrj['Ticket Number']
+#     ])
+
+
+
+
+
+
+
+
+
+
+
+############################################## 3) CSV Read ###################################################
+#with open('test.csv') as csvf:
+
+
+
+
+
+
--- GettingDataFrom_ConEd/jsonToCsv.py
+++ GettingDataFrom_ConEd/jsonToCsv.py
@@ -2,21 +2,26 @@
 # 2) read from a json file
 # 3) put the ticket numbers into a dictionary if it doesnt exist
 # 4) print that object into the csv file
-
-
-
 import json
 import csv
-with open("test.json") as file:
-    data_json = json.load(file)
+import pandas as pd
+# df = pd.read_json('test.json')
+# df.to_csv("test.csv")  #need to use two csv 
+# df.to_csv("test.csv") 
 
-# with open("test.csv", "w") as file:
-#     data_csv = csv.writer(file)
-#     for item in csv_file:
-#         fields = list(item['fields'].values())
-#         csv_file.writerow([item['pk'], item['model']] + fields)
+csvFileName = "test.csv"
+jsonFileName = "test.json"
 
+# If the csv is empty, print the header
+with open('test.csv', 'r') as csvfile:
+    csv_dict = [row for row in csv.DictReader(csvfile)]
+    if len(csv_dict) == 0:
+        csvHeader = ["Ticket Number","Longitude","Latitude","Zipcode","Classification","Date Reported"]
+        outf = csv.writer(open("test.csv","w", newline='')) 
+        outf.writerow(csvHeader)  
 
+#append the other stuff
+pd.read_json('test.json').drop(columns=['LastInspected']).to_csv("test.csv", mode='a', header=False) #append to csvuse ("test.csv", mode='a', header=False)
 
 
 
