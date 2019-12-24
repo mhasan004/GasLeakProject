55bf04939b1f0aafb9b8b8ea61e4a62070ef52e2
Mahmudul Hasan <mhasan0047@gmail.com>
2019-12-24 03:30:47 -0500

    making comments better

--- GettingDataFrom_ConEd/jsonToCsv.py
+++ GettingDataFrom_ConEd/jsonToCsv.py
@@ -1,23 +1,26 @@
+# Mahmudul Hasan. Script to scrape JSON Gas Leak Data points from ConEdison everyday and put them into a csv file for further use
 # 1) What tickets do we have? read the ticket col of csv of ticket txt file and add tickets to ticketList.txt
-# 2) anotherDict = contents of json file
+# 2) jsonDict = contents of json file
 # 3) Loop through the ticketNumber dictionary key and compare it to tickets in the ticketList.txt
     # 4) if the ticket exists, ignore it. If it dont exist 
-        #a) add the ticket to the dictionay.txt file and the tickeDictionary and push to end of csv
-        #b)
+        #a) add the ticket to the ticketList.txt and .csv files for storage and also add new ticket to the tickeSet
+
 import json
 import csv
 import pandas as pd     # to read the json and csv data
 import datetime         # to turn Microsoft JSON date /Date()/ to normal date
 import re               # to turn Microsoft JSON date /Date()/ to normal date
-def turnToDatetime(microsoftDate):          # Function to turn Microsoft JSOn date to mm/dd/yy and time
+
+# Function to turn Microsoft JSON date to mm/dd/yy and time:
+def turnToDatetime(microsoftDate):         
     TimestampUtc = str(microsoftDate)
     TimestampUtc = re.split('\(|\)', TimestampUtc)[1][:10]
     date = datetime.datetime.fromtimestamp(int(TimestampUtc))
     return str(date.strftime('%m/%d/20%y %I:%M %p')) # mm/dd/yyyy time am/pm
 
-
-csvFile = "UNION.csv"#"test.csv"                        # add to the end
-jsonFile = "ConEdGasLeakList_ManualRecords_UNION.json"#"test.json"                      # constant read (json web site)
+# Setting up variables:
+jsonFile = "ConEdGasLeakList_ManualRecords_UNION.json" # constantly reading from this file and adding new tickets to csv and txt files (will scrape from json web site)
+csvFile = "UNION.csv"                       # add new tickets to the end of the csv file
 ticketListFile = "ticketList.txt"           # add to end (just for me to see what i got)
 ticketSet = set()                           # need to add what i got in the csv atm
 jsonDict  = []                              # json file to dict: #jsonDict["TicketNumber/Long/lat/etc"][int index of the dot]) 
@@ -54,21 +57,24 @@ for row in range(0,len(csvData)):
 
 # 4) See if the ticket in "jsonDict" is in "ticketDict". If dont got add to "ticketDic", and .txt and .csv file for stoage. If got, skip this row since we have this info already. 
 for row in range(0, len(jsonDict)):
-    if jsonDict["TicketNumber"][row] not in ticketSet: # If we DONT have this ticket add it
+    if jsonDict["TicketNumber"][row] not in ticketSet:      # If we DONT have this ticket add it
         print("not in it do adding it")
         ticketSet.add(jsonDict["TicketNumber"][row])
-        outTXT.write(jsonDict["TicketNumber"][row]+"\n")     # add new ticket to txt file  
-        with open(csvFile,'a') as outCSV:               # Write the new Ticket object to csv file
+        outTXT.write(jsonDict["TicketNumber"][row]+"\n")    # add new ticket to txt file  
+        with open(csvFile,'a') as outCSV:                   # Write the new Ticket object to csv file
             s=""
-            for col in range(0, len(dotp)-1):           # go through each column/dot property
-                if dotp[col] == "DateReported":         # Need to change the Microsoft time to mm/dd/yyyy
+            for col in range(0, len(dotp)-1):               # go through each column/dot property
+                if dotp[col] == "DateReported":             # Need to change the Microsoft time to mm/dd/yyyy
                     s+=turnToDatetime(str(jsonDict[dotp[col]][row]))
                 else: 
                     s+=str(jsonDict[dotp[col]][row])
                 if col != len(dotp)-2:
                     s+=',' 
             s+="\n"
-            outCSV.write(s)                             # add new ticket obj to csv file         
+            outCSV.write(s)                                 # add new ticket obj to csv file  
+
+# 5) Commit chnages to gh
+       
 
 
 
