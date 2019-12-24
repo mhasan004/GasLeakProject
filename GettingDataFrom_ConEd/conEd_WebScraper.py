cc5ab97459d43cf29dc56a2b49587d713145fd06
Mahmudul Hasan <mhasan0047@gmail.com>
2019-12-24 17:46:57 -0500

    make scraper

--- GettingDataFrom_ConEd/conEd_WebScraper.py
+++ GettingDataFrom_ConEd/conEd_WebScraper.py
@@ -1,12 +1,21 @@
-from lxml import html
-import requests
+import requests                     # Getting html data
+from bs4 import BeautifulSoup       # Parse the HTML data
+import pandas as pd                 # 
 
-page = requests.get('https://apps.coned.com/gasleakmapweb/GasLeakMapWeb.aspx?ajax=true&')
-print(page)
+url = 'https://apps.coned.com/gasleakmapweb/GasLeakMapWeb.aspx?ajax=true&'
+res = requests.get(url)
+html_data = res.content
 
+soup = BeautifulSoup(html_data, 'html.parser')  # the HTML data to parse
+text = soup.find_all(text=True)
 
+jsonStr = ''                                     # turning to string
+for t in text:
+	jsonStr += '{} '.format(t)
 
-
+jsonScraped = []
+jsonScraped = pd.read_json(jsonStr, orient='records') # Turning the json string to a dictionary
+print(jsonScraped["TicketNumber"][0])
 
 
 
