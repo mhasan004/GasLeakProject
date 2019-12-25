9acf12e45673d8193ba4b171c709f1257c799d10
Mahmudul Hasan <mhasan0047@gmail.com>
2019-12-25 17:14:19 -0500

    small debug change

--- GettingDataFrom_ConEd/conEd_SceduledWebscraperJsonCSV.py
+++ GettingDataFrom_ConEd/conEd_SceduledWebscraperJsonCSV.py
@@ -31,8 +31,8 @@ properties= [                                                   # The JSON dot p
     "DateReported",
     "LastInspected"
 ]
-#PATH_OF_GIT_REPO = r'/home/pi/repositories/gh/GasLeakProject'  # the path to the .git file (this is the location on my raspberry pi)
-PATH_OF_GIT_REPO = '/home/hasan/repositories/gh/GasLeakProject' # the path to the .git file (this is the location on my Laptop)
+#PATH_OF_GIT_REPO = r'/home/pi/repositories/gh/GasLeakProject'  # the path to the .git file (.git location on my raspberry pi)
+PATH_OF_GIT_REPO = r'/home/hasan/repositories/gh/GasLeakProject' # the path to the .git file (.git location on my Laptop)
 COMMIT_MESSAGE = 'Automated Push - New Ticket Update'           # the commmit message when it is pushed
 ticketSet = set()                                               # need to add what i got in the csv atm
 jsonDict  = []                                                  # json file to dict: #jsonDict["TicketNumber/Long/lat/etc"][int index of the dot]) 
@@ -78,7 +78,7 @@ def WebscraperJsonToCSV():
     # 2) GET JSON DATA: from a JSON file and add to the JSON Dictionary: 
     # jsonDict = pd.read_json(jsonFile, orient='records')           # ***jsonDict[properties[i]/colStr(dot properties)][j/rowsnumber(dots)]
     
-    # 2) GET JSON DATA: Webscrape JSON data from the url and add to the JSON Dictionary: 
+    # 2) GET JSON DATA: Webscrape and sanitize the html response which is usually just the JSON data from the url and add to the JSON Dictionary: 
     res = requests.get(url)
     html_data = res.content                                             # Getting the HTML JSOn data 
     soup = BeautifulSoup(html_data, 'html.parser')                      # the HTML data to parse
@@ -92,7 +92,9 @@ def WebscraperJsonToCSV():
     except:
         print("Couldnt get the json data so will re-run function. This is Run "+ str(scrapingCount))
         print("***printing error jsonStr\n"+jsonStr)
+        print("\n***ENDED printing error jsonStr\n")
         print("***printing error jsonDict "+ str(jsonDict))
+        print("\n***ENDED printing error jsonDict \n")
         WebscraperJsonToCSV()
 
     # 3) CHECK WHAT TICKETS WE ALREADY GOT FROM THE .CSV FILE: Read the csv file and add "TicketNumbers" to the "ticketSet" and print ticketNumber to ticketList.txt" for storage: 
@@ -128,7 +130,7 @@ def WebscraperJsonToCSV():
 
 # 6) RESCAN FOR TICKETS every x time using sceduler
 scheduler = BlockingScheduler()
-scheduler.add_job(WebscraperJsonToCSV, 'interval', minutes=30)
+scheduler.add_job(WebscraperJsonToCSV, 'interval', minutes=10)
 scheduler.start()
 
 
