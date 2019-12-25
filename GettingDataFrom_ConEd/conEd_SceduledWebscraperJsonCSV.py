89b506335ebbd4471fdc8b45d4a4489a2a457b91
Mahmudul Hasan <mhasan0047@gmail.com>
2019-12-24 23:34:55 -0500

    Testing Automated Push - New Ticket Update

--- GettingDataFrom_ConEd/conEd_SceduledWebscraperJsonCSV.py
+++ GettingDataFrom_ConEd/conEd_SceduledWebscraperJsonCSV.py
@@ -116,7 +116,7 @@ def WebscraperJsonToCSV():
             git_push()
             isNewTicket = False
             print("***************** PUSHED for scrapingCount = " + str(scrapingCount))
-            break
+            continue
     print("Run Done " + str(scrapingCount))
 
 # Running the function every x seconds/minutes/hours
