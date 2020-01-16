f87875aeb14d6071716470a1dcb746f4cd606010
Mahmudul Hasan <mhasan0047@gmail.com>
2020-01-15 23:32:13 -0500

    fixed some issues with the count per census tact per hr csv

--- Getting_GasLeak_Data/building_newCensusCSV.py
+++ Getting_GasLeak_Data/building_newCensusCSV.py
@@ -14,9 +14,8 @@ censusTractCol = []
 countCol = []
 indexToSkip = []
 
+noRepeatSet = set()
 df = pd.read_csv(csvInFile) 
-print(len(df)) 
-s = ""
 for row in range(0,len(df)):
     date = df.iloc[row]["Date"]
     if row not in indexToSkip:                                                                     # A) Do this Date if i didnt do so already: 
@@ -31,24 +30,21 @@ for row in range(0,len(df)):
                 hourToSkip.extend(dateDF.index[dateDF["Hour"] == hour].tolist())                        # 2) adding the index of those targeted rows so can skip when we go down the row for this date   
                 
                 tractToSkip = []
+                s = ""
                 for rowTract in range(0, len(hourlyDF)):                                    
                     tract = hourlyDF.iloc[rowTract]["CensusTract"]          
                     if rowTract not in tractToSkip:                                                 # C) Do this Census Tract if i didnt do so already: 
                         tractDF = hourlyDF[hourlyDF.CensusTract == tract]                               # 1) new df = target rows (of those same hours, which were recorded in the same census tract?)                  
                         tractToSkip.extend(hourlyDF.index[hourlyDF["CensusTract"] == tract].tolist())   # 2) adding the index of those targeted rows so can skip when we go down the row for this hour
-                        s += tractDF.iloc[0]["Date"] + "," + str(tractDF.iloc[0]["Hour"]) + "," + str(tractDF.iloc[0]["CensusTract"]) + "," + str(len(tractDF)) + "\n"
-                        # print("###############################################################################")
-                        # print(tractDF)
-                        # print(
-                        #     "Date: " + str(tractDF.iloc[0]["Date"]) 
-                        #     + "     Hour: "        + str(tractDF.iloc[0]["Hour"])
-                        #     + "     CensusTract: " + str(tractDF.iloc[0]["CensusTract"] )
-                        #     + "     Count: "       + str(len(tractDF))
-                        # )
-                        # print("###############################################################################")
-print(s)
-with open(csvOutFile,'a') as outCSV:                           # Write the stuff to the csv file
-    outCSV.write(s)   
+                        if len(tractDF) >= 2:
+                            print("----------"+str(len(tractDF) ))
+                            d+=1
+                        s += tractDF.iloc[0]["Date"] + "," + str(tractDF.iloc[0]["Hour"]) + "," + str(tractDF.iloc[0]["CensusTract"]) + "," + str(len(tractDF)) # D) Index is 0 because im just counding how many there are in the df right now. Only need the Date, Hour, Tract, and count data
+                        noRepeatSet.add(s)
+                        s = ""
 
+with open(csvOutFile,'a') as outCSV:                                                                # Write the stuff to the csv file
+    for x in noRepeatSet:
+        outCSV.write(x+"\n")
 
-
+# there are only six 2's (manual check). If i had 783 before. And not i have 777. (783-777 = 6) so it all good
