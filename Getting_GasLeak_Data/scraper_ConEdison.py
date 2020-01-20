e293b63cac2e60974a4c034b6e741143b6ed4f2a
Mahmudul Hasan <mhasan0047@gmail.com>
2020-01-20 06:09:40 -0500

    Automated Push - New Ticket Update

--- Getting_GasLeak_Data/scraper_ConEdison.py
+++ Getting_GasLeak_Data/scraper_ConEdison.py
@@ -228,7 +228,7 @@ def WebscraperJsonToCSV():
 
 # 8) RESCAN FOR TICKETS every x time using sceduler
 scheduler = BlockingScheduler()
-scheduler.add_job(WebscraperJsonToCSV, 'interval', minutes=5) # need to give enough time to go the entire process
+scheduler.add_job(WebscraperJsonToCSV, 'interval', minutes=30) # need to give enough time to go the entire process
 scheduler.start()
 
 # a
