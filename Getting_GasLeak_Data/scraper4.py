81f8ff7663f55ccb9f2e7d42a7905a278586f5e7
Mahmudul Hasan <mhasan0047@gmail.com>
2020-01-20 22:21:38 -0500

    Automated Push - New Ticket Update

--- Getting_GasLeak_Data/scraper4.py
+++ Getting_GasLeak_Data/scraper4.py
@@ -30,8 +30,8 @@ url = 'https://apps.coned.com/gasleakmapweb/GasLeakMapWeb.aspx?ajax=true&'      
 dropCol = True                                                                                                      # If you want to drop a column, specify which ones in step 2 in WebscraperJsonToCSV()
 replaceColWith = ["Date", "Time", "Hour", "CensusTract", "CensusBlock", "CountyName" ]                              # Replacing column DateReported with these "Date", "Time", "Hour and Made 3 more cols for Part 2 Census data
 
-PATH_OF_GIT_REPO = r'/home/pi/repositories/gh/GasLeakProject'                                                       # the path to the .git file (.git location on my raspberry pi)
-# PATH_OF_GIT_REPO = r'/home/hasan/repositories/gh/GasLeakProject'                                                  # the path to the .git file (.git location on my Laptop)
+# PATH_OF_GIT_REPO = r'/home/pi/repositories/gh/GasLeakProject'                                                       # the path to the .git file (.git location on my raspberry pi)
+PATH_OF_GIT_REPO = r'/home/hasan/repositories/gh/GasLeakProject'                                                  # the path to the .git file (.git location on my Laptop)
 COMMIT_MESSAGE = 'Automated Push - New Ticket Update'                                                               # the commmit message when it is pushed
 scrapingCount = 0                                                                                                   # Just counting how many times i have scraped the website while this was running
 
@@ -192,9 +192,6 @@ def turnHourly_toMonthlyReport():
     with open(csvMonthlyFile,'a') as outCSV:                                                                        # Turning the DF into csv and appending the new data to the file
         outCSV.write(monthlyTractReportCountDF.to_csv(header=True, index=False))
 
-    
-    
-
 # THE SCHEDULER WILL RUN THIS MAIN FUNCTION EVER X SECONDS/MINUTES/HOURS
 def WebscraperJsonToCSV():  
     global scrapingCount                                                                                            # Setting up the web scraping global iteration counter for debugging purposes
@@ -268,7 +265,7 @@ def WebscraperJsonToCSV():
 
 # 8) RESCAN FOR TICKETS every x time using sceduler
 scheduler = BlockingScheduler()
-scheduler.add_job(WebscraperJsonToCSV, 'interval', minutes=30) # need to give enough time to go the entire process
+scheduler.add_job(WebscraperJsonToCSV, 'interval', seconds=10) # need to give enough time to go the entire process
 scheduler.start()
 
 
